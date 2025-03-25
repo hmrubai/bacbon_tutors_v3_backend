cc4e938a735441aad91f7424f1d27d9279a33c86
arfin foysal <arfinfoysalbd@gmail.com>
2025-03-25 14:15:33 +0600

    _is bookmaked_

--- app/Services/TutorJobService.php
+++ app/Services/TutorJobService.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth as FacadesJWTAuth;
 
 class TutorJobService
 {
@@ -18,14 +20,14 @@ class TutorJobService
 
     public function getByUserId($userId)
     {
-        return TutorJob::with(['medium', 'subjects', 'kid','grade','institutes','division','district','upazila','area'])
+        return TutorJob::with(['medium', 'subjects', 'kid', 'grade', 'institutes', 'division', 'district', 'upazila', 'area'])
             ->where('user_id', $userId)
             ->get();
     }
 
     public function getById($id)
     {
-        $tutorJob = TutorJob::with(['medium', 'subjects','institutes', 'kid','grade','division','district','upazila','area'])->findOrFail($id);
+        $tutorJob = TutorJob::with(['medium', 'subjects', 'institutes', 'kid', 'grade', 'division', 'district', 'upazila', 'area'])->findOrFail($id);
 
         return $tutorJob;
     }
@@ -38,7 +40,7 @@ class TutorJobService
         $tutorJob = TutorJob::create($data);
         $tutorJob->subjects()->sync($data['subject_ids']);
         $tutorJob->institutes()->sync($data['institute_ids']);
-        
+
 
         return $tutorJob;
     }
@@ -48,8 +50,8 @@ class TutorJobService
         $job = TutorJob::findOrFail($id);
         unset($data['user_id']);
         $job->update($data);
-            $job->subjects()->sync($data['subject_ids']);
-            $job->institutes()->sync($data['institute_ids']);
+        $job->subjects()->sync($data['subject_ids']);
+        $job->institutes()->sync($data['institute_ids']);
         return $job;
     }
 
@@ -62,80 +64,87 @@ class TutorJobService
 
 
     public function allJobs(Request $request): Collection|LengthAwarePaginator|array
-{
-    $query = TutorJob::query();
-    $query->with(['medium', 'subjects', 'kid', 'institutes','grade']);  // Add 'institutes' here
-    $query->select(['*']);
+    {
 
-    // Sorting
-    $this->applySorting($query, $request);
 
-    // Searching
-    $searchKeys = ['job_title'];
-    $this->applySearch($query, $request->input('search'), $searchKeys);
+        $query = TutorJob::query();
+        $query->with(['medium', 'subjects', 'kid', 'institutes', 'grade']);  // Add 'institutes' here
+        $query->select(['*']);
 
-    // Filters (single-value filters)
-    $filters = [
-        'tuition_type',
-        'division_id',
-        'district_id',
-        'upazila_id',
-        'gender',
-        'negotiable',
-    ];
+        // this is an open api but i want to check is auth
+        $query->selectRaw('CASE WHEN EXISTS (SELECT 1 FROM tuition_bookmarks WHERE tutor_job_id = tutor_jobs.id AND user_id = ?) THEN 1 ELSE 0 END AS is_bookmark', [auth()->id()]);
 
-    foreach ($filters as $filter) {
-        if ($request->has($filter)) {
-            $query->where($filter, $request->input($filter));
-        }
-    }
+        // Sorting
+        $this->applySorting($query, $request);
 
-    // Multi-value filters
-    $multiValueFilters = [
-        'area_ids' => 'area_id',
-        'medium_ids' => 'medium_id',
-        'class_ids' => 'grade_id',
-        'institute_ids' => 'institutes', // Filter by related institutes
-        'subject_ids' => 'subjects', // Filter by related subjects
-    ];
+        // Searching
+        $searchKeys = ['job_title'];
+        $this->applySearch($query, $request->input('search'), $searchKeys);
 
-    foreach ($multiValueFilters as $requestParam => $relation) {
-        if ($request->has($requestParam)) {
-            $values = explode(',', $request->input($requestParam));
-            if ($relation === 'institutes') {
-                // Filter by related institutes (belongsToMany)
-                $query->whereHas('institutes', function ($query) use ($values) {
-                    $query->whereIn('institute_id', $values);
-                });
-            } elseif ($relation === 'subjects') {
-                // Filter by related subjects (belongsToMany)
-                $query->whereHas('subjects', function ($query) use ($values) {
-                    $query->whereIn('subject_id', $values);
-                });
-            } else {
-                // Handle other cases for direct columns or special cases
-                $query->whereIn($relation, $values);
+        // Filters (single-value filters)
+        $filters = [
+            'tuition_type',
+            'division_id',
+            'district_id',
+            'upazila_id',
+            'gender',
+            'negotiable',
+        ];
+
+        foreach ($filters as $filter) {
+            if ($request->has($filter)) {
+                $query->where($filter, $request->input($filter));
             }
         }
-    }
 
-    // Salary range filter
-    if ($request->has('salary_amount')) {
-        $salaryRange = explode(',', $request->input('salary_amount'));
-        if (count($salaryRange) === 2) {
-            $query->whereBetween('salary_amount', [$salaryRange[0], $salaryRange[1]]);
+        // Multi-value filters
+        $multiValueFilters = [
+            'area_ids' => 'area_id',
+            'medium_ids' => 'medium_id',
+            'class_ids' => 'grade_id',
+            'institute_ids' => 'institutes', // Filter by related institutes
+            'subject_ids' => 'subjects', // Filter by related subjects
+        ];
+
+        foreach ($multiValueFilters as $requestParam => $relation) {
+            if ($request->has($requestParam)) {
+                $values = explode(',', $request->input($requestParam));
+                if ($relation === 'institutes') {
+                    // Filter by related institutes (belongsToMany)
+                    $query->whereHas('institutes', function ($query) use ($values) {
+                        $query->whereIn('institute_id', $values);
+                    });
+                } elseif ($relation === 'subjects') {
+                    // Filter by related subjects (belongsToMany)
+                    $query->whereHas('subjects', function ($query) use ($values) {
+                        $query->whereIn('subject_id', $values);
+                    });
+                } else {
+                    // Handle other cases for direct columns or special cases
+                    $query->whereIn($relation, $values);
+                }
+            }
         }
-    }
 
-    // Pagination
-    return $this->paginateOrGet($query, $request);
-}
+        // Salary range filter
+        if ($request->has('salary_amount')) {
+            $salaryRange = explode(',', $request->input('salary_amount'));
+            if (count($salaryRange) === 2) {
+                $query->whereBetween('salary_amount', [$salaryRange[0], $salaryRange[1]]);
+            }
+        }
+
+
+
+        // Pagination
+        return $this->paginateOrGet($query, $request);
+    }
 
 
 
     public function jobDetailsByID($id)
     {
-        return TutorJob::with(['medium', 'subjects', 'kid', 'institutes','grade','division','district','upazila','area'])->findOrFail($id);
+        return TutorJob::with(['medium', 'subjects', 'kid', 'institutes', 'grade', 'division', 'district', 'upazila', 'area'])->findOrFail($id);
     }
 
     public function bookmarkTutorJob($id)
