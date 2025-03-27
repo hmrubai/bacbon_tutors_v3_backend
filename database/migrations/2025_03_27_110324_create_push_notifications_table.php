539c23f41994d2ef0094b48512fdd5b05cdaf99d
hmrubai <rubai.mobarak@gmail.com>
2025-03-27 16:04:24 +0600

    Push notification has been added

--- database/migrations/2025_03_27_110324_create_push_notifications_table.php
+++ database/migrations/2025_03_27_110324_create_push_notifications_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('push_notifications', function (Blueprint $table) {
             $table->id();
+            $table->text('title');
+            $table->text('details');
+            $table->string('thumbnail')->nullable();
+            $table->bigInteger('user_id');
+            $table->string('redirection_path')->nullable();
+            $table->json('data')->nullable();
+            $table->boolean('is_for_all')->default(0);
+            $table->boolean('is_read')->default(0);
+            $table->timestamp('read_at')->nullable();
+            $table->bigInteger('created_by')->nullable();
             $table->timestamps();
         });
     }
